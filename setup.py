2f5cd91070805982464a050ee70753b54d24414e
JP <fiorini751@gmail.com>
2018-01-27 10:50:38 -0500

    - updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dirgen',
-    version='0.4',
+    version='0.5',
     py_modules=['dirgen'],
     license="MIT",
     install_requires=[

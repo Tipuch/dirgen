f312cb5198e447bb42d3a5f6f73f8bf349a7c74d
JP <fiorini751@gmail.com>
2018-01-26 22:48:56 -0500

    - changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dirgen',
-    version='0.3',
+    version='0.4',
     py_modules=['dirgen'],
     license="MIT",
     install_requires=[
